0546324a57fda5b8cb344b064b7bb77984c8b53e
Bj-LAI <bjornlai13@gmail.com>
2024-12-28 11:53:50 +0100

    Ajout de l'input pour la camera

--- src/PlayerInput.ts
+++ src/PlayerInput.ts
@@ -10,6 +10,12 @@ export class PlayerInput {
     public jumpKeyDown: boolean;
     public hoverKeyDown: boolean;
 
+    // Camera values
+    public camVertical : number;
+    public camHorizontal: number;
+    public camVerticalAxis: number;
+    public camHorizontalAxis: number;
+
 
     constructor(scene: Scene) {
         scene.actionManager = new ActionManager();
@@ -30,29 +36,29 @@ export class PlayerInput {
 
     private _updateFromKeyboard(): void {
         if (this.inputMap["ArrowUp"]) {
-            this.vertical = Scalar.Lerp(this.vertical, 1, 0.2);
-            this.verticalAxis = 1;
+            this.camVertical = Scalar.Lerp(this.camVertical, 1, 0.2);
+            this.camVerticalAxis = 1;
 
         } else if (this.inputMap["ArrowDown"]) {
-            this.vertical = Scalar.Lerp(this.vertical, -1, 0.2);
-            this.verticalAxis = -1;
+            this.camVertical = Scalar.Lerp(this.camVertical, -1, 0.2);
+            this.camVerticalAxis = -1;
 
         } else {
-            this.vertical = 0;
-            this.verticalAxis = 0;
+            this.camVertical = 0;
+            this.camVerticalAxis = 0;
         }
 
         if (this.inputMap["ArrowLeft"]) {
-            this.horizontal = Scalar.Lerp(this.horizontal, -1, 0.2);
-            this.horizontalAxis = -1;
+            this.camHorizontal = Scalar.Lerp(this.camHorizontal, -1, 0.2);
+            this.camHorizontalAxis = -1;
 
         } else if (this.inputMap["ArrowRight"]) {
-            this.horizontal = Scalar.Lerp(this.horizontal, 1, 0.2);
-            this.horizontalAxis = 1;
+            this.camHorizontal = Scalar.Lerp(this.camHorizontal, 1, 0.2);
+            this.camHorizontalAxis = 1;
 
         } else {
-            this.horizontal = 0;
-            this.horizontalAxis = 0;
+            this.camHorizontal = 0;
+            this.camHorizontalAxis = 0;
         }
 
         // Jump Checks (SPACE)
@@ -66,6 +72,32 @@ export class PlayerInput {
             this.hoverKeyDown = true;
         } else {
             this.hoverKeyDown = false;
+        }
+
+        if (this.inputMap["z"] || this.inputMap["Z"]) {
+            this.vertical = Scalar.Lerp(this.vertical, 1, 0.2);
+            this.verticalAxis = 1;
+
+        } else if (this.inputMap["s"] || this.inputMap["S"]) {
+            this.vertical = Scalar.Lerp(this.vertical, -1, 0.2);
+            this.verticalAxis = -1;
+
+        } else {
+            this.vertical = 0;
+            this.verticalAxis = 0;
+        }
+
+        if (this.inputMap["q"] || this.inputMap["Q"]) {
+            this.horizontal = Scalar.Lerp(this.horizontal, -1, 0.2);
+            this.horizontalAxis = -1;
+
+        } else if (this.inputMap["d"] || this.inputMap["D"]) {
+            this.horizontal = Scalar.Lerp(this.horizontal, 1, 0.2);
+            this.horizontalAxis = 1;
+
+        } else {
+            this.horizontal = 0;
+            this.horizontalAxis = 0;
         }
 
     }
