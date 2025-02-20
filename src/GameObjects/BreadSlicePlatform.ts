cbdc9ca3449e990edfc40b53f31ce7b3831c6a93
Bj-LAI <bjornlai13@gmail.com>
2025-02-20 11:35:45 +0100

    Rotation de la tranche de pain

--- src/GameObjects/BreadSlicePlatform.ts
+++ src/GameObjects/BreadSlicePlatform.ts
@@ -3,17 +3,28 @@ import {Vector3} from "@babylonjs/core";
 
 export class BreadSlicePlatform extends GameObject {
     private _states = ["rotating", "moving"];
-    private _currentState = "moving";
+    private _currentState = "rotating";
     private _speed: Vector3 = new Vector3(0, 0, 0.5);
+    private _rotationSpeed: number = 0.2;
 
 
     update(): void {
-        if (this._currentState === "moving") {
-            this._move();
+        switch(this._currentState) {
+            case "moving":
+                this._move();
+                break;
+            case "rotating":
+                this._rotate();
+                break;
         }
+
     }
 
     private _move() {
         this._mesh.position.addInPlace(this._speed.scale(this._game.getPlayer().getDeltaTime()));
+    }
+
+    private _rotate() {
+        this._mesh.rotation.x += this._rotationSpeed * this._game.getPlayer().getDeltaTime();
     }
 }
