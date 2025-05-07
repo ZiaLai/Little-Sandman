6ba0183a970fc460f98118ca117a79dfae3fcd57
ziala <zia.lai@etu.univ-amu.fr>
2025-05-07 08:12:35 +0200

    lumieres dans chambre boulangère

--- src/Levels/BakersBedroom.ts
+++ src/Levels/BakersBedroom.ts
@@ -1,6 +1,6 @@
 import {AbstractLevel} from "./AbstractLevel";
 import {Game} from "../game";
-import {ActionManager, Scene, Vector3} from "@babylonjs/core";
+import {ActionManager, Color3, HemisphericLight, PointLight, Scene, Vector3} from "@babylonjs/core";
 
 export class BakersBedroom extends AbstractLevel {
     private _startPosition: Vector3;
@@ -8,7 +8,7 @@ export class BakersBedroom extends AbstractLevel {
     constructor(game: Game, id: number) {
         super(game, id);
         this._name = "bakers_bedroom";
-        this._ressourceName = "baker_bedroom v3";
+        this._ressourceName = "baker_bedroom v4";
         this._startPosition = new Vector3(3.74, 0.91, -3.28);
 
     }
@@ -41,10 +41,15 @@ export class BakersBedroom extends AbstractLevel {
     }
 
     protected setUpLights(): void {
-        // TODO implement (sun et point de lumiere sur la lampe de chevet)
-    }
+        var light0 = new HemisphericLight("berdroom_light", new Vector3(0, 1, 0), this._game.getGameScene());
+        light0.diffuse = new Color3(35/255,67/255,131/255);
+        light0.intensity = 0.3;
+        // LOUPIote
+        const light = new PointLight("loupiote", new Vector3(4, 2.8, 1), this._game.getGameScene());
+        light.diffuse = new Color3(222/255,193/255,223/255);
+        light.intensity =10;    }
 
     protected setUpSkydome(): void {
-        // TODO implement
+
     }
 }
