6658956eb219c03b1c2c38a3c92d71aab061b953
ziala <zia.lai@etu.univ-amu.fr>
2025-05-07 15:30:47 +0200

    début gui compteur d'éléments cauchemardesques trouvés + débur lumières

--- src/Levels/SugarlessBakery.ts
+++ src/Levels/SugarlessBakery.ts
@@ -1,20 +1,22 @@
 import {AbstractLevel} from "./AbstractLevel";
 import {Environment} from "../environment";
 import {Game} from "../game";
-import {Scene} from "@babylonjs/core";
+import {Color3, HemisphericLight, PointLight, Scene, Vector3} from "@babylonjs/core";
+import {AdvancedDynamicTexture, Control, Image} from "@babylonjs/gui";
 
 
 export class SugarlessBakery extends AbstractLevel{
-
+    nb_nightmare_found = 0;
     constructor(game: Game, id: number) {
         super(game, id);
         this._name = "sugarless_bakery";
-        this._ressourceName = "bakery_level_1";
+        this._ressourceName = "bakery_level_02";
     }
 
     protected async load() {
         super.load();
         // todo : charger la musique
+        this.setUpGui();
     }
 
     update(): void {
@@ -34,9 +36,33 @@ export class SugarlessBakery extends AbstractLevel{
 
     protected setUpLights(): void {
         // TODO add lights sur les gateaux + lumiere rouge dans le four
+        var light0 = new HemisphericLight("nightLight", new Vector3(0, 1, 0), this._game.getGameScene());
+        light0.diffuse = new Color3(0.066,0.082,1);
+        light0.intensity = 0.5;
+        const light = new PointLight("four", new Vector3(4, 2.8, 1), this._game.getGameScene());// TODO CHANGE POS
+        light.diffuse = new Color3(1,0,0.5);
+        light.intensity =50;
+        const cake1 = new PointLight("cake 1", new Vector3(4, 2.8, 1), this._game.getGameScene());// TODO CHANGE POS
+        cake1.diffuse = new Color3(0.585, 1, 0.573);
+        cake1.intensity =10;
+
     }
 
     protected setUpSkydome(): void {
         // TODO implement (esce qu'on peut changer de texture losqu'on corrige un element ? )
+    }
+    private setUpGui(): void {
+        const ui = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+        const compteur = new Image("compteur", "/textures/compteur-"+this.nb_nightmare_found+".png");
+        compteur.width = "25%";
+        compteur.height = "25%";
+        compteur.stretch = Image.STRETCH_UNIFORM;
+        compteur.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+
+        compteur.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+        compteur.paddingLeft = 40;
+        ui.addControl(compteur);
+
     }
 }
