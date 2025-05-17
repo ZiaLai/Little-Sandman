08ba7c383e58b9c16efb81c9cb83923355aab7c3
Bj-LAI <bjornlai13@gmail.com>
2025-05-17 18:57:02 +0200

    Debut dde spawn pos dans le skatepark

--- src/Levels/CityLevel.ts
+++ src/Levels/CityLevel.ts
@@ -1,18 +1,24 @@
 import {AbstractLevel} from "./AbstractLevel";
 import {Game} from "../game";
-import {ActionManager, ExecuteCodeAction, Mesh, Scene, SetValueAction} from "@babylonjs/core";
+import {ActionManager, ExecuteCodeAction, Mesh, Scene, SetValueAction, Vector3} from "@babylonjs/core";
 import {IntroLoopMusic} from "../AudioControl/IntroLoopMusic";
 import {BakersBedroom} from "./BakersBedroom";
+import {Music} from "../AudioControl/Music";
 
 export class CityLevel extends AbstractLevel{
+
+    private _skateparkMusic: Music;
+    public static SKATEPARK_SPAWN_POSITION: Vector3 = new Vector3(20, -45, 378);
 
     constructor(game: Game, id: number) {
         super(game, id);
         this._name = "city";
-        this._ressourceName = "city_v20";
+        this._ressourceName = "city";
 
         this._music = new IntroLoopMusic(this._game.getScene(), [ ['intro', './musics/city/city_intro.ogg'],
                                                                   ['loop', './musics/city/city_loop.ogg'  ] ]);
+
+        // this._skateparkMusic = new LoopMusic(this._game.)
     }
 
     protected async load() {
@@ -28,7 +34,7 @@ export class CityLevel extends AbstractLevel{
     }
 
     initialize(): void {
-
+        //this._game.getPlayer().setPosition(CityLevel.SKATEPARK_SPAWN_POSITION);
     }
 
     update(): void {
