514c3b4e3edf02b01e1e7e7a5ec1aab5ece7fcac
RyoLucifer <florentgilloteaux@gmail.com>
2025-05-19 19:19:28 +0200

    Changement portée du lancer de sable

--- src/ShootingSystem.ts
+++ src/ShootingSystem.ts
@@ -33,7 +33,7 @@ export class ShootingSystem {
 
     public getRayFromShooting(scene: Scene, position: Vector3, direction: Vector3): void {
         if (this._isShooting) {
-            this._shootingRay = new Ray(position, direction, 2);
+            this._shootingRay = new Ray(position, direction, 3);
             let rayHelper = new RayHelper(this._shootingRay);
             rayHelper.show(scene);
         }
