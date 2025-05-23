ad6f0b8aedeeb9757567e0d470953908eb942260
ziala <zia.lai@etu.univ-amu.fr>
2025-05-23 11:28:45 +0200

    default value

--- src/SpawnData.ts
+++ src/SpawnData.ts
@@ -5,6 +5,8 @@ export class SpawnData {
     public readonly direction: Vector3;
     public readonly alpha: number;
 
+    public static DEFAULT_VALUE: SpawnData = new SpawnData(new Vector3(0, 3, 0), new Vector3(0, 0, 0), 0);
+
     constructor(position: Vector3, direction: Vector3, alpha: number) {
         this.position = position;
         this.direction = direction;
