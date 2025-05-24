8bda2350d12affb983da1b73fb0b087206e622b9
ziala <zia.lai@etu.univ-amu.fr>
2025-05-24 10:13:42 +0200

    correction fade text : ajout de promesses

--- src/util/FadeText.ts
+++ src/util/FadeText.ts
@@ -2,32 +2,40 @@ import {TextBlock} from "@babylonjs/gui";
 
 export class FadeText{
     public static async fadeOut(textBlock: TextBlock, duration: number = 2000) {
-        textBlock.alpha = 1;
-        const step = 16;
-        const totalSteps = duration / step;
-        let currentStep = 0;
+        return new Promise<void>((resolve) => {
 
-        const interval = setInterval(() => {
-            currentStep++;
-            textBlock.alpha = Math.max(1 - currentStep / totalSteps, 0);
+            textBlock.alpha = 1;
+            const step = 16;
+            const totalSteps = duration / step;
+            let currentStep = 0;
 
-            if (currentStep >= totalSteps) {
-                clearInterval(interval);
-            }
-        }, step);
+            const interval = setInterval(() => {
+                currentStep++;
+                textBlock.alpha = Math.max(1 - currentStep / totalSteps, 0);
+
+                if (currentStep >= totalSteps) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, step);
+        });
     }
     public static async fadeIn(textBlock: TextBlock, duration: number = 2000) {
-        textBlock.alpha = 0;
-        const step = 16; // environ une frame (~60fps)
-        const totalSteps = duration / step;
-        let currentStep = 0;
-        const interval = setInterval(() => {
-            currentStep++;
-            textBlock.alpha = Math.min(currentStep / totalSteps, 1);
+        return new Promise<void>((resolve) => {
+            textBlock.alpha = 0;
+            const step = 16;
+            const totalSteps = duration / step;
+            let currentStep = 0;
 
-            if (currentStep >= totalSteps) {
-                clearInterval(interval);
-            }
-        }, step);
+            const interval = setInterval(() => {
+                currentStep++;
+                textBlock.alpha = Math.min(currentStep / totalSteps, 1);
+
+                if (currentStep >= totalSteps) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, step);
+        });
     }
 }
