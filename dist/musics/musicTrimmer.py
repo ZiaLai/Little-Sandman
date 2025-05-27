fdfda1ff61e5d3e5bd6a0a565820c84da340a759
RyoLucifer <florentgilloteaux@gmail.com>
2025-05-28 01:06:36 +0200

    Ajout des assets dans dist/

---
+++ dist/musics/musicTrimmer.py
@@ -0,0 +1,39 @@
+from pydub import AudioSegment
+import os
+
+def cut_audio(input_file, output_file, start_time, end_time):
+    """
+    Cut parts of an audio file and save it to a new file.
+
+    Args:
+        input_file (str): Path to the input audio file.
+        output_file (str): Path to save the output audio file.
+        start_time (int): Start time in milliseconds.
+        end_time (int): End time in milliseconds.
+    """
+    # Load the audio file
+    audio = AudioSegment.from_file(input_file)
+
+    # Cut the audio
+    cut_audio = audio[start_time:end_time]
+
+    # Save the cut audio to a new file
+    cut_audio.export(output_file, format="ogg")
+
+
+FOLDER = "city"
+# Durée en millisecondes
+DURATION = 76000
+
+def folder_cut():
+    for filename in os.listdir(FOLDER):
+        input_file = FOLDER + "/" + filename
+        output_file = FOLDER + "/" + filename
+        start_time = 0  # Start at 10 seconds
+        end_time = DURATION  # End at 30 seconds
+        cut_audio(input_file, output_file, start_time, end_time)
+
+
+# folder_cut()
+
+# cut_audio("city/city_loop.ogg", "city/city_loop.ogg", 0, 152000)
