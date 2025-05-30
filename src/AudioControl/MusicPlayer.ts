45b9cd4d038b57fbacdc385e88276cfd235a324a
RyoLucifer <florentgilloteaux@gmail.com>
2025-05-30 14:30:22 +0200

    Renommage des fichiers ressources pour pourvoir crée le lien jsdelivr (les espaces, accents, etc... m'empeche de créer le lien)

--- src/AudioControl/MusicPlayer.ts
+++ src/AudioControl/MusicPlayer.ts
@@ -26,8 +26,8 @@ export class MusicPlayer {
             new Sound("piano1", "./musics/sugarlessBakery/sugarless_bakery-Piano_1.ogg", this._app.getScene(), soundReady, {loop : true}),
             new Sound("piano2", "./musics/sugarlessBakery/sugarless_bakery-Piano_2.ogg", this._app.getScene(), soundReady, {loop : true}),
             new Sound("bass", "./musics/sugarlessBakery/sugarless_bakery-Basse.ogg", this._app.getScene(), soundReady, {loop : true}),
-            new Sound("flute", "./musics/sugarlessBakery/sugarless_bakery-Flûte.ogg", this._app.getScene(), soundReady, {loop : true}),
-            new Sound("guitar", "./musics/sugarlessBakery/sugarless_bakery-Guitare_électrique.ogg", this._app.getScene(), soundReady, {loop : true}),
+            new Sound("flute", "./musics/sugarlessBakery/sugarless_bakery-Flute.ogg", this._app.getScene(), soundReady, {loop : true}),
+            new Sound("guitar", "./musics/sugarlessBakery/sugarless_bakery-Guitare_electrique.ogg", this._app.getScene(), soundReady, {loop : true}),
             new Sound("battery", "./musics/sugarlessBakery/sugarless_bakery-Set_de_batterie.ogg", this._app.getScene(), soundReady, {loop : true}),
             new Sound("violins", "./musics/sugarlessBakery/sugarless_bakery-Violons.ogg", this._app.getScene(), soundReady, {loop : true})
         ]
